b8fbe54d278167fdc64fbe668bce3757d07ac037
Al-hafiz Academy App <itsranas517@gmail.com>
2021-10-06 17:39:41 +0500

    Add Browse and Active Storage functionality

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'webpacker', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'devise'
-
+gem "aws-sdk-s3", require: false
 
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Account.create([
+                 {first_name: "Anas0", last_name: "Shahbaz0", username: "anas0", email: "anas0@gmail.com", password: "password"},
+                 {first_name: "Anas1", last_name: "Shahbaz1", username: "anas1", email: "anas1@gmail.com", password: "password"},
+                 {first_name: "Anas2", last_name: "Shahbaz2", username: "anas2", email: "anas2@gmail.com", password: "password"},
+                 {first_name: "Anas3", last_name: "Shahbaz3", username: "anas3", email: "anas3@gmail.com", password: "password"},
+                 {first_name: "Anas4", last_name: "Shahbaz4", username: "anas4", email: "anas4@gmail.com", password: "password"}
+
+               ])
